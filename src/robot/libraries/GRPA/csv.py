da7252efe0bfc7f684270f72763bf3e74983c7fa
Pairat2542 <konggmhw@gmail.com>
2024-05-07 11:29:56 +0700

    fix encode csv bug

--- src/robot/libraries/GRPA/csv.py
+++ src/robot/libraries/GRPA/csv.py
@@ -1,7 +1,7 @@
 import csv
 
 def _write_list_to_csv(file_name=None,data=None,encode=None):
-    if str(encode).lower == 'csv':
+    if str(encode).lower() == 'csv':
         with open(file_name, 'w',newline='') as file:
             writer = csv.writer(file)
             writer.writerows(data)
